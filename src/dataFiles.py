50fddc51757e213716f9be5907078fde55529245
PedroMDAndre <pedro.mda@gmail.com>
2021-07-05 13:34:25 +0100

    load save contacts

--- src/dataFiles.py
+++ src/dataFiles.py
@@ -1,9 +1,10 @@
 path: str = "../save/contacts.csv"
 separator: str = ","
+contactos_test: list[list[str]] = [['Jane', ' 123', ' 234', ' jane@outlook.com'],
+                              ['Paul', ' 153', ' 134', ' jane@outlook.com']]
 
 
-def saveContactsData():
-    contactos = [['Jane', ' 123', ' 234', ' jane@outlook.com'], ['Paul', ' 153', ' 134', ' jane@outlook.com']]
+def saveContactsData(contactos: list[list[str]]):
     ficheiro = open(path, "w", encoding="utf-8-sig")
 
     for contacto in contactos:
@@ -13,17 +14,18 @@ def saveContactsData():
     ficheiro.close()
 
 
-def loadContactsData():
+def loadContactsData() -> list[list[str]]:
     pessoas = []
     ficheiro = open(path, "r", encoding="utf-8-sig")
     linhas = ficheiro.readlines()
     ficheiro.close()
 
     for linha in linhas:
-        pessoa = linha.replace("\n", "").split(",")
+        pessoa = linha.replace("\n", "").split(separator)
         pessoas.append(pessoa)
     print(pessoas)
+    return pessoas
 
 
 if __name__ == '__main__':
-    saveContactsData()
+    saveContactsData(loadContactsData())
