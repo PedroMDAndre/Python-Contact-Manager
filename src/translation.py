290bfdec5064242597593e8a070b9570208aabd7
PedroMDAndre <pedro.mda@gmail.com>
2021-07-05 14:30:28 +0100

    tradução

---
+++ src/translation.py
@@ -0,0 +1,11 @@
+dictionary: dict = {
+    "add_contact": {"Adicionar Contacto", "Add Contact"},
+    "remove_contact": {"Remover Contacto", "Remove Contact"},
+    "sort_contacts": {"Ordenar Cres./Decres.", "Sort Asc./Des."},
+    "find_contacts": {"Procurar", "Find"},
+    "show_all_entries": {"Todas os contactos", "All Contacts"},
+    "change_language": {"Mudar de Língua", "Change Language"}
+}
+
+PT: int = 0
+EN: int = 1
